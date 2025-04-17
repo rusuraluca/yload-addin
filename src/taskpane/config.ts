5294b2d562323e7a90c594f04676e4d338e92023
Raluca Rusu <rr.ralucarusu@gmail.com>
2025-04-17 14:33:32 +0100

    Update config.ts

--- src/taskpane/config.ts
+++ src/taskpane/config.ts
@@ -5,21 +5,12 @@ const getEnvUrl = (devUrl, prodUrl) => {
 
 const config = {
     // Base URLs for the application
-    baseUrl: getEnvUrl(
-        process.env.DEV_URL,
-        process.env.PROD_URL
-    ),
+    baseUrl: process.env.DEV_URL,
 
     // Yload API endpoints for authentication
-    authUrl: getEnvUrl(
-        process.env.DEV_AUTH_URL,
-        process.env.PROD_AUTH_URL
-    ),
+    authUrl:process.env.DEV_AUTH_URL,
 
-    apiBaseUrl: getEnvUrl(
-        process.env.DEV_BASE_URL,
-        process.env.PROD_BASE_URL
-    ),
+    apiBaseUrl: process.env.DEV_BASE_URL,
 };
 
 export default config;
